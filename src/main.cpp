0eee8d24ef495d8312626be832dc320a56444623
Chrysophylaxs <pierterbijl2@gmail.com>
2021-04-06 22:17:15 +0200

    Position class constructor and layout

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,23 @@ int main() {
 	// for (Square s = A1; s <= H8; s++)
 	// 	std::cout << bb_string(ROOK_MASKS[s]) << std::endl;
 
-	std::cout << bb_string(queen_moves(E5, RANK_2 | RANK_6 | FILE_B | FILE_F)) << std::endl;
+	// std::cout << bb_string(queen_moves(A1, 0)) << std::endl;
+
+	// for (int i = 0; i < NUM_CASTLING; i++)
+	// 	std::cout << bb_string(BB_CASTLING[i]) << std::endl;
+
+	Position p = Position();
+	std::cout << p << std::endl;
+
+	for (Color c : {WHITE, BLACK})
+		for (PieceType pt : {PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING})
+			
+			std::cout << bb_string(p.pieces[c][pt]) << std::endl;
+
+	for (Color c : {WHITE, BLACK})
+		std::cout << bb_string(p.colors[c]) << std::endl;
+
+	std::cout << bb_string(p.all_pieces) << std::endl;
 
 	return 0;
 }
